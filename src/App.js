5e80df313545b3028f4bd86693a252db0bc5287a
brianonyango <brian.ogango@student.moringaschool.com>
2023-03-28 22:17:51 +0300

    add auth provider to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React,{useState} from 'react';
+import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import Layout from './components/layout/Layout';
@@ -7,29 +7,28 @@ import Login from './components/Login';
 import Events from './components/Events';
 import Register from './components/Register';
 import Details from './components/Details';
+import New from './components/New';
 import AuthProvider from './components/context/AuthContext';
 
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(
-		sessionStorage.getItem("isLoggedIn") === "true"
-	);
+ 
   return (
-
-    <BrowserRouter >
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Home />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/register' element={<Register />}/>
-        <Route path='/events' element={<Events />}/>
-        <Route path='/details/:id' element={<Details />}/>
-        <Route path='/new' element={<New />} />
-      </Route>
-    </Routes>
-      
-    </BrowserRouter>
-  );
+		<BrowserRouter>
+			<AuthProvider>
+				<Routes>
+					<Route path='/' element={<Layout />}>
+						<Route index element={<Home />} />
+						<Route path='/login' element={<Login />} />
+						<Route path='/register' element={<Register />} />
+						<Route path='/events' element={<Events />} />
+						<Route path='/details/:id' element={<Details />} />
+						<Route path='/new' element={<New />} />
+					</Route>
+				</Routes>
+			</AuthProvider>
+		</BrowserRouter>
+	);
 
 	
 }

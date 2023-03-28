1f990d20c15df4ccc39c6a672c119e75558013b9
brianonyango <brian.ogango@student.moringaschool.com>
2023-03-28 15:14:23 +0300

    create event component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import Layout from './components/layout/Layout';
-import Login from './components/Login';
-import Tickets from './components/Tickets';
 import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -14,8 +13,6 @@ function App() {
         <Route index element={<Home />}/>
         <Route path='/login' element={<Login />}/>
         <Route path='/register' element={<Register />}/>
-        <Route path='/tickets' element={<Tickets />}/>
-
       </Route>
     </Routes>
       

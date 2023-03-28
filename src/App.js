2ba0a55d5ade73dc003643ff0832cfefb9acdd4d
Steve <stevekoech8@gmail.com>
2023-03-28 15:59:57 +0300

    added events

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import Layout from './components/layout/Layout';
 import Login from './components/Login';
 import Register from './components/Register';
-import Upcoming from './components/Upcoming';
+import Events from './components/Events';
 
 function App() {
   return (
@@ -13,14 +13,10 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />}/>
         <Route path='/login' element={<Login />}/>
-
-        <Route path='/new' element={<Register />}/>
-        <Route path='/events' element={<Upcoming />}/>
-
+        <Route path='/events' element={<Events />}/>
         <Route path='/register' element={<Register />}/>
       </Route>
-    </Routes>
-      
+    </Routes> 
     </BrowserRouter>
   );
 }
